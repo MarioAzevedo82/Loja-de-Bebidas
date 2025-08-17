57652a01918a5048f445a4d945bd073c9295b34f
Mario Azevedo <mariobct@msn.com>
2025-08-17 10:50:21 -0300

    JS adicionando ao carrinho

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ let carrinho = []
 const botoes = document.querySelectorAll(".adicionar")
 const contador = document.getElementById("carrinho-contador")
 
+botoes.forEach(botao => {
+    botao.addEventListener("click", (e) => {
+        const produto = e.target.parentElement.querySelector("h2").textContent
+        const preco = e.target.parentElement.querySelector("p").textContent
+
+        carrinho.push({produto,preco})
+
+        contador.textContent = carrinho.length
+
+        alert(`${produto} adicionado ao carrinho!`)
+    })
+})
